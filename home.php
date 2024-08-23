17072683d4e04cd2a3815e605e25c36ea1920c25
ducanhvp <nhda90@gmail.com>
2024-08-23 13:00:19 +0700

    init project

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "hello ";
+    echo '<h1 style="color: green; text-align:center;">Xin chào</h1>';
+    ?>
+</body>
+</html>
